652480fd6d8624e3f128583f1b1c124bfcebea93
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-23 01:18:24 +0200

    add task detail page routing and make header global

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import {Projects} from './components/Projects';
 import {Header} from './Header.js';
-import {ProjectDetailPage} from './components/projectDetailPage.js'
+import {ProjectDetailPage} from './components/projectDetailPage.js';
+import {TaskDetailPage} from './components/TaskDetailPage.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetailPage/>} />
+        <Route path="/project/:id/:task_id" element={<TaskDetailPage/>} />
       </Routes>
     </div>
   );
@@ -18,7 +21,6 @@ function App() {
 function Home() {
   return (
     <>
-      <Header />
       <Projects />
     </>
   );

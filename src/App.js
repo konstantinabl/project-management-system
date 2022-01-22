4885ed872792fec25b74982c194e82f77809039f
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-22 17:45:32 +0200

    Create basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import {Projects} from './components/Projects'
+import {Header} from './Header.js'
 
 function App() {
   return (
     <div className="App">
-      <Projects />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Projects />
+    </>
   );
 }
 

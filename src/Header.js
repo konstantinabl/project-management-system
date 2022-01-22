1b252755052fe8d341b84bb93467abf3d0a0aa96
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-22 17:38:44 +0200

    create Header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+      <>
+       <div className="header">
+            <h1>Project Management System</h1>
+        </div>
+      </>
+    );
+  };
+  
+  export default Header

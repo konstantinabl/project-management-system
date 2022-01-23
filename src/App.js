84913ddf45880b179542ec3a2d34cb4efbf04640
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-23 03:09:43 +0200

    Load json into local storage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import {Projects} from './components/Projects';
 import {Header} from './Header.js';
-import {ProjectDetailPage} from './components/projectDetailPage.js';
+import {ProjectDetailPage} from './components/ProjectDetailPage.js';
 import {TaskDetailPage} from './components/TaskDetailPage.js';
+import data_json from "./data/data.json";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(() => {
+    const saved = localStorage.getItem("data");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data_json));
+  }, [data]);
+
   return (
     <div className="App">
       <Header />

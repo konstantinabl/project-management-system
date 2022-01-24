7f20ccd217ce3bcbccfe62ab84cd51599f5d4b51
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-25 00:56:37 +0200

    add search functionality to task page

--- src/components/projectDetailPage.js
+++ src/components/projectDetailPage.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import { Task } from './Task.js';
 import { useParams } from "react-router-dom";
 
 export const ProjectDetailPage = ({data, setData}) => {
     const params = useParams();
+    const [keyword, setKeyword] = useState(null);
     
+    const currentProject = data.find(data => data.Name == params.id);
+    const filteredTasks = currentProject.Backlog.filter(task => {
+        if(!keyword) return task;
+
+        return task.Title.toLowerCase().includes(keyword.toLowerCase()) || task.Description.toLowerCase().includes(keyword.toLowerCase());
+    });
+
     return (
         <div className="container">
+            <input value={keyword} onChange={e => setKeyword(e.target.value)}/>
             <table className="project-detail-table">
             <thead>
                 <tr>
@@ -14,21 +24,14 @@ export const ProjectDetailPage = ({data, setData}) => {
                 </tr>
             </thead>
             <tbody>
-            {data.filter(data => data.Name == params.id).map((data) => {
-                const tasks = data.Backlog
-                return(
-                    tasks.map((task) => {
-                        return(
-                            <Task
-                                title={task.Title}
-                                status={task.Status}
-                                projectName={data.Name}
-                                clickable={true}
-                            />
-                        )
-                    })
-                )
-            })}
+            {filteredTasks.map((task) => (
+                <Task
+                    title={task.Title}
+                    status={task.Status}
+                    projectName={data.Name}
+                    clickable={true}
+                />
+            ))}
             </tbody>
             </table>
         </div>

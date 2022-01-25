dae5f2b019f81eefa508608d00d7c1edc6ee001b
konstantinabl <konstantina.blajukova@gmail.com>
2022-01-25 13:31:57 +0200

    wrap project name in Link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,20 @@
 import React from "react";
-import data from "../data/data.json";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Projects = ({data}) => {
-  let navigate = useNavigate();
-
-  const handleClick = (name) => {
-    navigate(`/project/${name}`);
-  }
   return (
     <>
       <div className="project-container">
-        {data.map((data) => {
+        {data.map((project) => {
           return (
-            <div key={data.Name}>
+            <div key={project.Name}>
               <table className="main-page-table">
                 <tbody>
                   <tr>
                     <td>
-                      <h5 onClick={()=> {
-                        handleClick(data.Name)
-                      }} >{data.Name}</h5>
+                      <Link to={"/project/" + project.Name}>
+                        <h5>{project.Name}</h5>
+                      </Link>
                     </td>
                   </tr>
                 </tbody>
